a7942915bd0f34b663c5f9678ae182f9be7b747b
5antia60 <santiago.delgado2001@outlook.com>
2025-05-07 14:28:04 -0300

    refactor(race): added car param

--- src/app/fundamentals/dip.ts
+++ src/app/fundamentals/dip.ts
@@ -1,8 +1,22 @@
 import race from '@/core/fundamentals/race';
 import TerminalUtil from '../utils/terminalUtil';
+import Fusca from '@/core/fundamentals/fusca';
+import Ferrari from '@/core/fundamentals/ferrari';
+import Civic from '@/core/fundamentals/civic';
 
 export default async function Dip() {
   TerminalUtil.title('DIP');
-  race();
+
+  const [type] = await TerminalUtil.select('Tipo de carro?', ['Fusca', 'Civic', 'Ferrari']);
+
+  let car;
+  if (type === 0)
+    car = new Fusca();
+  else if (type === 1)
+    car = new Civic();
+  else
+    car = new Ferrari();
+
+  race(car);
   await TerminalUtil.waitEnter();
 }
--- src/core/fundamentals/race.ts
+++ src/core/fundamentals/race.ts
@@ -1,9 +1,7 @@
 import { terminal } from 'terminal-kit';
-import Fusca from './fusca';
+import Car from './car';
 
-export default function race() {
-  const car = new Fusca();
-
+export default function race(car: Car): void {
   Array.from({ length: 10 }).forEach(() => {
     car.accelerate();
     terminal.red('\nVelocidade: ' + car.currentSpeed);
